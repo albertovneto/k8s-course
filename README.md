b49cd0feee855e8053e56a05be1f1c7488d04379
albertovieira <vieira900@hotmail.com>
2022-07-03 22:28:54 -0300

    Added config-map

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cursos
+# K8S
+### -  Cria o configmap
+    kubectl apply -f k8s/configmap-family.yaml
+
+### - Comando para entrar no bash do pod
+    kubectl exec -it pode-name -- bash
+
+### - Comando para ver logs do pod 
+    kubectl logs pod-name
--- server.go
+++ server.go
@@ -3,10 +3,12 @@ import (
 	"net/http"
 	"os"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/", ConfigMap)
 	http.ListenAndServe(":80", nil)
 }
 
@@ -14,4 +16,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+    data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+
+    if err != nil {
+        log.Fatalf("Error reading file: ", err)
+    }
+
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
